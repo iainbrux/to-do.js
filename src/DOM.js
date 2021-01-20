8443974bce989591663956efdc64bd1a68b6cc3e
iainbrux <iain.brookes@outlook.com>
2021-01-20 01:27:02 +0000

    further additions to js and css

--- src/DOM.js
+++ src/DOM.js
@@ -1,10 +1,45 @@
-function render() {
+import Project from "./projects";
 
+let projects = [];
+
+export function render() {
+    let button = document.querySelector('.add-new');
+    button.addEventListener('click', () => displayModal())
+
+    let x = document.querySelector('.x');
+    x.addEventListener('click', () => exitModal())
+
+    let submitButton = document.querySelector('.submit-button');
+    submitButton.addEventListener('click', () => submit());
 }
 
-function createModal() {
-    let content = document.querySelector('.container')
-    let modal = document.querySelector('.modal');
-
-
+function exitModal() {
+    let modal = document.querySelector('.bg-modal');
+    modal.style.display = "none"
 }
+
+function displayModal() {
+    let modal = document.querySelector('.bg-modal');
+    modal.style.display = "flex";
+}
+
+function submit() {
+    
+    const newProject = new Project();
+
+    let name = document.querySelector('#name').value;
+    let desc = document.querySelector('#desc').value;
+    let due = document.querySelector('#due').value;
+    let status = document.querySelector('#status').value;
+
+    newProject.projectName = name;
+    newProject.projectDesc = desc;
+    newProject.projectDate = due;
+    newProject.projectStatus = status; 
+
+    projects.push(newProject);
+
+    console.log(projects);
+}
+
+export { projects }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import Task from './tasks.js'
 import Project from './projects.js'
+import { render, projects } from './DOM'
+
+render();
