e1bd87f88a78aa54e690da58db741a5479da12bc
iainbrux <iain.brookes@outlook.com>
2021-01-28 11:53:19 +0000

    begin re-write of js functions

--- src/DOM.js
+++ src/DOM.js
@@ -3,64 +3,60 @@ import Project from "./projects";
 let projects = [];
 
 export function render() {
-    let button = document.querySelector('.add-new');
-    button.addEventListener('click', () => displayModal())
-}
+    let newTaskButton = document.querySelector('.submit');
+    newTaskButton.addEventListener('click', () => submit())
 
-function exitModal() {
-    let modal = document.querySelector('.bg-modal');
-    modal.style.display = "none"
-}
-
-function displayModal() {
-    let modal = document.querySelector('.bg-modal');
-    modal.style.display = "flex";
+    deleteFromTasks();
 }
 
 function submit() {
 
     const newProject = new Project();
 
-    let name = document.querySelector('#name').value;
-    let desc = document.querySelector('#desc').value;
-    let due = document.querySelector('#due').value;
-    let status = document.querySelector('#status').value;
+    let name = document.querySelector('#task').value;
+    let due = document.querySelector('#due-date').value;
 
     newProject.projectName = name;
-    newProject.projectDesc = desc;
     newProject.projectDate = due;
-    newProject.projectStatus = status;
 
     projects.push(newProject);
 
-    (function addProjectToDOM() {
+    // (function addProjectToDOM() {
 
-        let list = document.querySelector('.list');
+    //     let list = document.querySelector('.list');
 
-        let projectDiv = document.createElement('div')
-        let titleDiv = document.createElement('div')
-        let descriptionDiv = document.createElement('div')
-        let dueDiv = document.createElement('div')
-        let statusDiv = document.createElement('div')
+    //     let projectDiv = document.createElement('div')
+    //     let titleDiv = document.createElement('div')
+    //     let descriptionDiv = document.createElement('div')
+    //     let dueDiv = document.createElement('div')
+    //     let statusDiv = document.createElement('div')
 
-        projectDiv.classList.add('project');
-        titleDiv.classList.add('title');
-        descriptionDiv.classList.add('description');
-        dueDiv.classList.add('due');
-        statusDiv.classList.add('status');
+    //     projectDiv.classList.add('project');
+    //     titleDiv.classList.add('title');
+    //     descriptionDiv.classList.add('description');
+    //     dueDiv.classList.add('due');
+    //     statusDiv.classList.add('status');
 
-        titleDiv.innerText = newProject.name;
-        descriptionDiv.innerText = newProject.desc;
-        dueDiv.innerText = newProject.due;
-        statusDiv.innerText = newProject.status;
+    //     titleDiv.innerText = newProject.name;
+    //     descriptionDiv.innerText = newProject.desc;
+    //     dueDiv.innerText = newProject.due;
+    //     statusDiv.innerText = newProject.status;
 
-        projectDiv.append(titleDiv, descriptionDiv, dueDiv, statusDiv);
+    //     projectDiv.append(titleDiv, descriptionDiv, dueDiv, statusDiv);
 
-        list.append(projectDiv);
+    //     list.append(projectDiv);
 
-    })();
+    // })();
 
     console.log(projects);
+}
+
+function deleteFromTasks() {
+    let tasks = document.querySelectorAll('.task');
+    tasks.forEach(task => {
+        let deleteBtn = task.querySelector('.delete');
+        deleteBtn.addEventListener('click', () => task.remove());
+    });
 }
 
 export {
--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import Project from './projects.js'
 import { render, projects } from './DOM'
 
 render();
+
+let tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task => {
+    let deleteBtn = task.querySelector('.delete');
+    deleteBtn.addEventListener('click', () => {
+        task.innerHTML = "";
+    })
+})
+
