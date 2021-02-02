631ade81841cbcf131ea9fccf476affc991e8bdd
iainbrux <iain.brookes@outlook.com>
2021-02-02 21:31:04 +0000

    Completed saving data logic to localstorage

--- src/submit.js
+++ src/submit.js
@@ -1,16 +1,20 @@
 import Task from "./Task";
 import addTaskToDOM from './renderTask';
 
-//This function extends to DOM.js but due to the size was seperated into it's own file
+//This function extends to DOM.js but due to the size was seperated into it's own file.
+//I'll be honest, this piece of code took me almost 20 hours to figure.
+//It's janky AF but it does what I need it to do exactly how I need it to do it. DO NOT TOUCH IT!
+//NO SERIOUSLY, DO NOT TOUCH IT!
 
 let appData = {};
 let projectName = document.querySelector('.project-title');
-window[`${projectName.innerText}Array`] = [];
+window[`${projectName.innerText}Array`] = []; //Creates a dynmaically named variable that is an empty array.
 
 export default function submit() {
 
     let temp = document.querySelector('.project-title').innerText;
     let retrievedJSON = JSON.parse(localStorage.getItem(temp));
+    console.log('Retrieved JSON:')
     console.log(retrievedJSON)
 
     const newTask = new Task();
@@ -20,22 +24,25 @@ export default function submit() {
 
     newTask[name.value] = {
         due: due.value,
-        status: null
+        status: false
     };
 
     appData[projectName.innerText] = null;
 
-    //I'll be honest, this piece of code took me almost 20 hours to figure, it's janky AF but it does what I need it to do exactly how I need it to do it. DO NOT TOUCH IT!
+    //From here onwards, leave this alone.
 
     for (let key in appData) {
         if (key == projectName.innerText) {
-            console.log('Success');
             if (window[`${key}Array`] == undefined) {
                 let key = projectName.innerText;
                 window[`${key}Array`] = [];
             }
-            window[`${key}Array`].push(newTask);
-            console.log(window[`${key}Array`]);
+            if (retrievedJSON == null) {
+                retrievedJSON = [];
+            }
+            retrievedJSON.push(newTask);
+            console.log(`JSON Array:`)
+            console.log(retrievedJSON);
             addTaskToDOM(newTask);
             return addToLocalStorage();
         }
@@ -44,7 +51,7 @@ export default function submit() {
     function addToLocalStorage() {
         appData[projectName.innerText] = window[`${projectName.innerText}Array`];
 
-        let objectJSON = JSON.stringify(appData[projectName.innerText]);
+        let objectJSON = JSON.stringify(retrievedJSON);
         localStorage.setItem(projectName.innerText, objectJSON);
 
         name.value = "";
